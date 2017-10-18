516f175f371046394f72ebf2f4880ab5b3d63ae7
Fabricio Catae <fcatae@microsoft.com>
2017-10-17 22:28:31 -0200

    add memory allocator

---
+++ src/BotHub/BotContext/Memory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotHub.BotContext
+{
+    public class Memory<T> : IDisposable
+    {
+        public T Value { get; set; }
+
+        public Memory(T value)
+        {
+            this.Value = value;
+        }
+
+        public void Dispose()
+        {
+            this.Value = default(T);
+        }
+    }
+}
---
+++ src/BotHub/BotContext/MemoryAllocator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotHub.BotContext
+{
+    public class MemoryAllocator        
+    {
+        public Memory<T> Alloc<T>() 
+            where T : new()
+        {
+            return new Memory<T>(new T());
+        }
+
+        public Memory<T> Load<T>(string value)
+        {
+            T memoryObject = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(value);
+
+            return new Memory<T>(memoryObject);
+        }
+
+        public string Save<T>(Memory<T> memory)
+        {
+            string serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(memory.Value);
+
+            return serializedObject;
+        }
+
+        //public void Free<T>(Memory<T> memory)
+        //{
+        //    memory.Dispose();
+        //}
+    }
+}
